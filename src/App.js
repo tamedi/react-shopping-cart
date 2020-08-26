f4a290ba1a4f8a60f4503b3ea36a42b366677082
tamedi <tam.ilias22000@gmail.com>
2020-08-26 13:47:17 +0200

    cart-finish

--- src/App.js
+++ src/App.js
@@ -14,6 +14,12 @@ class App extends React.Component {
       sort: "",
     };
   }
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter((x) => x._id !== product._id),
+    });
+  };
   addToCart = (product) => {
     const cartItems = this.state.cartItems.slice();
     let alreadyInCart = false;
@@ -87,7 +93,10 @@ class App extends React.Component {
               ></Products>
             </div>
             <div className="sidebare">
-              <Cart cartItems={this.state.cartItems} />
+              <Cart
+                cartItems={this.state.cartItems}
+                removeFromCart={this.removeFromCart}
+              />
             </div>
           </div>
         </main>
